f0faa7f73f6e0824d3f39c6130bf3e85faa27e5d
KennySinners <odeukuracsve@gmail.com>
2019-08-22 10:57:05 +0200

    More commands owo

--- src/bot/typescript/config.ts
+++ src/bot/typescript/config.ts
@@ -1,12 +1,12 @@
 import { con } from "./configurations";
 
 export let config = {
-    "token": con.token,
-    "prefix": "?",
-    "commands": {
-        "admin": ["ban", "kick", "tempban", "massban"],
-        "mod": ["mute", "unmute", "tempmute"],
-        "eco": ["balance"],
-        "util": ["help", "test", "userinfo", "serverinfo"]
+    token: con.token,
+    prefix: "?",
+    commands: {
+        admin: ["ban", "kick", "tempban", "massban"],
+        mod: ["mute", "unmute", "tempmute"],
+        eco: ["balance"],
+        util: ["help", "test", "userinfo", "serverinfo"]
     }
 };
