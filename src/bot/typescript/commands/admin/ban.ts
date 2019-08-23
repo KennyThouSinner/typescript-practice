49cd06ffedb6dbe8d80373b5817472d565d4d5c1
KennySinners <odeukuracsve@gmail.com>
2019-08-23 20:32:39 +0200

    Assigning new values to a variable dependend on if there is a mention or not

--- src/bot/typescript/commands/admin/ban.ts
+++ src/bot/typescript/commands/admin/ban.ts
@@ -1,5 +1,6 @@
 import { Message, Client, MessageEmbed } from "discord.js";
 import { IBotCommand } from "../../api";
+import { parse } from "path";
 
 export default class ban implements IBotCommand {
 
@@ -27,33 +28,35 @@ export default class ban implements IBotCommand {
 
     async runCommand(args: string[], message: Message, client: Client): Promise<void> {
 
-        const member = message.mentions.members.first() || await message.guild.members.fetch(args[0]);
+        let member;
+
+        message.mentions.users.size >= 1 
+        ? member = message.guild.member(message.mentions.users.first())
+        : member = await message.guild.members.fetch(args[0]);
+        
         const reason = args.slice(1).join(" ") || "No Reason";
 
         if (!args[0]) {
-            message.channel.send("Please provide a member to ban.")
+            message.channel.send("Please provide a member to ban");
             return;
         }
 
-        if (!message.guild.members.fetch(args[0])) {
-            message.channel.send("That member is not inside of the guild.")
+        if (args[0] && !isNaN(parseInt(args[0])) && !(await message.guild.members.fetch(args[0]))) {
+            message.channel.send("The provided member ID did not match any member inside of the guild.");
             return;
         }
 
-        if (member && message.guild.members.get(member.id)) {
-            const banEmbed = new MessageEmbed()
-                .setColor("WHITE")
-                .setTitle(member.displayName + " was banned from " + member.guild.name)
-                .setAuthor(member.displayName, member.user.displayAvatarURL())
-                .addField(`Member who was banned in ${message.guild.name}`, member.displayName)
-                .addField(`Member who banned ${member.displayName}`, message.member.displayName)
-                .setFooter(message.guild.me.displayName, client.user.displayAvatarURL())
-                .setTimestamp();
+        const banEmbed = new MessageEmbed()
+            .setColor("WHITE")
+            .setTitle(member.displayName + " was banned from " + member.guild.name)
+            .setAuthor(member.displayName, member.user.displayAvatarURL())
+            .addField(`Member who was banned in ${message.guild.name}`, member.displayName)
+            .addField(`Member who banned ${member.displayName}`, message.member.displayName)
+            .setFooter(message.guild.me.displayName, client.user.displayAvatarURL())
+            .setTimestamp();
 
-            member.ban({ days: 2, reason: reason }).then(ban => {
-                message.channel.send(banEmbed);
-            })
-        }
-
+        member.ban({ days: 2, reason: reason }).then(ban => {
+            message.channel.send(banEmbed);
+        });
     }
 }
