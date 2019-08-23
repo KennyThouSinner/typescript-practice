e712904d115db38d2aa1af65d890a3db70287ec2
KennySinners <odeukuracsve@gmail.com>
2019-08-24 01:56:18 +0200

    Removed unnecessary imports

--- src/bot/typescript/index.ts
+++ src/bot/typescript/index.ts
@@ -1,12 +1,6 @@
-import { Message, Client, Guild, Collection } from "discord.js";
+import { Client, Guild } from "discord.js";
 import { IBotCommand } from "./api";
-import { config } from "./config";
-import Server, { GuildModel } from "./assets/mongoose/schemas/Guild";
 import * as mongoose from "mongoose";
-import { Model, model } from "mongoose";
-import Balance, { BalanceModel } from "./assets/mongoose/schemas/balance";
-import * as fs from "fs";
-import * as chalk from "chalk";
 import { Member } from "./classes/member";
 import { loader } from "./classes/loader";
 import { superClient } from "./classes/superClient";
