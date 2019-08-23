6f852c575c4bde5a655628435815f0dad991b0c6
KennySinners <odeukuracsve@gmail.com>
2019-08-23 20:32:07 +0200

    Only implemented one loader that loads all commands from all categories

--- src/bot/typescript/classes/loader.ts
+++ src/bot/typescript/classes/loader.ts
@@ -8,17 +8,18 @@ class loader {
 
         if (!config || Object.keys(config.commands).length <= 0) { return; }
 
-        let z = 0;
 
         for (let i of Object.keys(config.commands)) {
 
-            const cmdclass = require(`${path}/${i}/${(config.commands[i])[z]}`).default;
+            for (let z in config.commands[i]) {
 
-            const cmd = new cmdclass() as IBotCommand;
-            
-            commands.push(cmd);
+                const cmdclass = require(`${path}/${i}/${(config.commands[i])[z]}`).default;
+
+                const cmd = new cmdclass() as IBotCommand;
+
+                commands.push(cmd);
+            }
         };
-        z++
     };
 };
 
