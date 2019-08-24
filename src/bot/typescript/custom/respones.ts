b2adbe6151211052cbe349ab9e3c859f8aee3833
KennySinners <odeukuracsve@gmail.com>
2019-08-24 06:37:19 +0200

    Added an 'initial_response' property

--- src/bot/typescript/custom/respones.ts
+++ src/bot/typescript/custom/respones.ts
@@ -1,7 +1,7 @@
 import { arrayOfResponses } from "./arrayresponses";
 
 export let responses = {
-
     missing_permissions: arrayOfResponses[0],
-    invalid_member: arrayOfResponses[1]
+    invalid_member: arrayOfResponses[1],
+    initial_response: arrayOfResponses[2]
 }
