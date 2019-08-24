775df8970ac22776e9e5db127e9e93e20896b3c3
KennySinners <odeukuracsve@gmail.com>
2019-08-24 11:07:30 +0200

    New additions to the HelpHandler

--- src/bot/typescript/classes/muteHelpHandler.ts
+++ src/bot/typescript/classes/muteHelpHandler.ts
@@ -1,20 +1,20 @@
-import { Message, CollectorFilter } from "discord.js";
+import { Message, CollectorFilter, MessageEmbed } from "discord.js";
 import { responses } from "../custom/respones";
+import { commands } from "../index";
+import { IBotCommand } from "../api";
+import { GenericMessageEmbedPageHandler } from "../generichRichEmbedPageHandler";
 
 class HelpHandler {
     message: Message;
-    filter: CollectorFilter;
     args: Array<String>;
 
     /** 
      * @param message 
-     * @param filter 
      * @param args 
      */
 
-    constructor(message, filter, args) {
+    constructor(message, args) {
         this.message = message;
-        this.filter = filter;
         this.args = args;
     }
 
@@ -27,12 +27,13 @@ class HelpHandler {
      * }
      */
 
-    public Respond = async () => {
+    public Respond = async (filter: CollectorFilter) => {
+
         if (this.args[0].toLowerCase() === "help") {
             if (!this.args[1]) {
                 this.message.channel.send(responses.initial_response)
                     .then(m => {
-                        this.message.channel.awaitMessages(this.filter, { max: 1 })
+                        this.message.channel.awaitMessages(filter, { max: 1 })
                             .then(async collected => {
                                 if (collected.first().content.toLowerCase().includes("no")) {
                                     await this.message.delete();
@@ -52,6 +53,68 @@ class HelpHandler {
                     this.message.channel.send(responses.invalid_member);
                 }
             }
+        }
+    }
+
+    public helpRespond = async () => {
+
+        if (!this.message.content.split(" ").slice(1)[0]) {
+            let embed = new MessageEmbed()
+                .setTitle("List of all the commands");
+
+            let sent = (await this.message.channel.send(embed)) as Message;
+
+            if (Array.isArray(this.message)) {
+                this.message = this.message[0];
+            }
+
+            let itemHandler = (embed: MessageEmbed, data: Array<IBotCommand>) => {
+                data.forEach(item => {
+                    embed.addField(`${item._commandKeyWords[0][0].toUpperCase() + item._commandKeyWords[0].slice(1)}`, `${item.help()} | Usage: ${item.usage()} || Admin Only: ${item.adminOnly()}`);
+                })
+                return embed;
+            }
+
+            let handler = new GenericMessageEmbedPageHandler<IBotCommand>(commands, 5, itemHandler, embed, sent)
+
+            return handler.startCollecting(this.message.author.id, sent);
+
+        } else {
+            const foundCmd = commands.find(arr => arr._commandKeyWords[0].toLowerCase() === this.message.content.split(" ").slice(1)[0].toLowerCase());
+
+            return this.message.channel.send(`Command: \`\`${foundCmd._commandKeyWords[0].toUpperCase() + foundCmd._commandKeyWords[0][0].slice(1)}\`\`, \nAliases: \`${foundCmd._commandKeyWords.slice(1).length >= 1 ? foundCmd._commandKeyWords.slice(1) : "No aliases for this command!"}\`, \nUsage: \`\`${foundCmd.usage()}\`\`,
+\n\`Admin Only\`: 
+\`\`\`js \n
+${foundCmd.adminOnly()}
+\`\`\`
+\`Developer Only\`: 
+\`\`\`js \n
+${foundCmd.devOnly()}
+\`\`\` `);
+        }
+    }
+
+    public aliasesRespond = () => {
+
+        const embed = new MessageEmbed().setTimestamp();
+        const foundCmd = commands.find(arr => arr._commandKeyWords[0].toLowerCase() === this.args[0].toLowerCase());
+        const foundAlias = commands.find(arr => arr._commandKeyWords.slice(1).some(keyword => keyword.toLowerCase() === this.args[0].toLowerCase()))
+
+        if (foundCmd) {
+            embed
+                .setTitle(`Aliases for \`\`${foundCmd._commandKeyWords[0]}\`\` `)
+            !(foundCmd._commandKeyWords.slice(1).length <= 0) ? embed.addField(`Aliases:`, ` \`\`${foundCmd._commandKeyWords.slice(1).join('\n')}\`\` `) : embed.addField("Aliases: ", "No aliases for this command!")
+            this.message.channel.send(embed);
+            return;
+        } else if (foundAlias) {
+
+            this.message.channel.send(`I didn't find the original command name, but I found it's alias... \nOriginal Command Name: \n**\`\`${foundAlias._commandKeyWords[0]}\`\`** \nIt's aliases: \n\`\`${foundAlias._commandKeyWords.slice(1).join("\n")}\`\` `)
+
+            return;
+        } else if (!foundCmd && !foundAlias) {
+
+            this.message.channel.send(`I couldn't find that command, sorry!`);
+            return;
         }
     }
 }
