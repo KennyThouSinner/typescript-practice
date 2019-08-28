01c21b86f5f7c3da7166ca372d703cf5d8dd98eb
KennySinners <odeukuracsve@gmail.com>
2019-08-28 04:56:26 +0200

    New replies / small additions

--- src/bot/typescript/commands/admin/softban.ts
+++ src/bot/typescript/commands/admin/softban.ts
@@ -31,19 +31,18 @@ export default class softban implements IBotCommand {
             const mentionedMember = message.mentions.members.first() || await message.guild.members.fetch(args[0]);
 
             if (!message.member.permissions.has("BAN_MEMBERS")) {
-                  message.channel.send(`You do not have the sufficient permission.`);
+                  message.reply('you do not have the sufficient permission to execute this action');
                   return;
             }
 
-            if (!mentionedMember && args[0]) {
+            if (!args[0]) {
                   message.channel.send("You must either mention a member or supply a member ID");
                   return;
             }
-            
+
             new Member(client, {}, message.guild)
                   .softban({ id: mentionedMember.id }).then(es => {
                         message.channel.send(`Successfully soft-banned ${mentionedMember.user.tag} from ${message.guild.name}. Removed messages that are ${es} day(s) old`)
-                        console.log(es);
                   })
       };
 };
